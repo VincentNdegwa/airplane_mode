cbdb5921ff653cf3692dc36413032b8c185ec626
VincentNdegwa <ndegwavincent7@gmail.com>
2024-08-11 23:44:16 +0300

    flight business

---
+++ airplane_mode/airplane_mode/doctype/flight_business/flight_business.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, vincent ndegwa and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Flight Business", {
+// 	refresh(frm) {
+
+// 	},
+// });
